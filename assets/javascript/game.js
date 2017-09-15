a7980d3544f03f74af9656d1e962da3d50c6c411
Steven McCreery <logad8@gmail.com>
2017-09-15 10:39:03 -0400

    changed the files around and added content

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+
+// starting with hangman as a new object
+var Hangman = new Object() {
+
+	// defining variables within the object constructor notation
+	var remainingGuess = 8;
+
+	var words = ["thing", "stuff", "Third"];
+
+	var currentGuess = function() {
+		.getElementById();
+	}
+
+	var checkGuess = function() {
+		;
+	}
+
+}
+
+
+
+
+
