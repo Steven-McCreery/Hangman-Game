34356806fba7ff9d699f99b6eb9208b0d54d6f87
Steven McCreery <logad8@gmail.com>
2017-09-16 02:45:50 -0400

    tinkering with js, not much effect but starting to add what should work in and trying to fix as I go

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,28 @@
 
-// starting with hangman as a new object
-var Hangman = new Object() {
+var remainingGuesses = 8;
 
-	// defining variables within the object constructor notation
-	var remainingGuess = 8;
+remainingGuesses = document.getElementById("guesses");
 
-	var words = ["thing", "stuff", "Third"];
+guesses = function () {
+    guesses.appendChild = remainingGuesses;
+    if (guesses < 1) {
+      guesses.innerHTML = "Game Over";
+    }
+  }
 
-	var currentGuess = function() {
-		.getElementById();
-	}
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t", "u", "v", "w", "x", "y", "z"];
 
-	var checkGuess = function() {
-		;
-	}
-
+play = function () {
+    words = ["lettuce", "pickles", "onion", "cheese", "ketchup", "mustard", "mayonnaise"];
+    currentWord = words[Math.floor(Math.random() * words.length)];
 }
+
+play();
+
+document.getElementById("word").innerHTML(words);
+
+
+
 
 
 
