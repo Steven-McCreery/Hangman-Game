9cd2ad6cafe3d0bc6320d1ecba51b9baac112d31
Steven McCreery <logad8@gmail.com>
2017-09-16 04:41:26 -0400

    getElementById is giving lots of problems in js code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,53 @@
 
-var remainingGuesses = 8;
-var initialWin = getElementById(wins).innerHTML(0);
-var initialLoss = getElementById(losses).innerHTML(0);
-var initialAttempt = getElementById(attempts).innerHTML(0);
+var remainingGuesses;
+var initialWin = 0;
+var initialLoss;
+var initialAttempt;
+
+// var remainingGuesses = 8;
+// var initialWin = document.getElementById("wins").innerHTML(0);
+// var initialLoss = document.getElementById("losses").innerHTML(0);
+// var initialAttempt = document.getElementById("attempts").innerHTML(0);
 
 remainingGuesses = document.getElementById("guesses");
 
-guesses = function () {
-    guesses.appendChild = remainingGuesses;
-    if (guesses < 1) {
-      guesses.innerHTML = "Game Over";
-      getElementById(losses).innerHTML(losses++);
-    }
-  }
 
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t", "u", "v", "w", "x", "y", "z"];
 
 play = function () {
     words = ["lettuce", "pickles", "onion", "cheese", "ketchup", "mustard", "mayonnaise"];
     currentWord = words[Math.floor(Math.random() * words.length)];
-    console.log(word);
+    console.log(currentWord);
+    var currentWordLength = currentWord.length;
+	var underscores = "";
+	for(i=0; i<currentWordLength; i++) {
+	    underscores = underscores + "_ ";
+	}
+    console.log(underscores);
+    document.getElementById("word").append("<div>underscores</div>");
+}
+
+play();
+
+
+userGuess = function() {
+	onkeyup()
+	guesses = function () {
+	    guesses.appendChild = remainingGuesses;
+	    if (guesses < 1) {
+	      guesses.innerHTML = "Game Over";
+	      getElementById(losses).innerHTML(losses++);
+	    }
+	  }
 }
 
 
-document.getElementById("button").addEventListener("click", function()
-	{
-	console.log("abcdefg");
-	}
-);
+document.getElementById("button").onclick = function(){
+	var initialWin = document.getElementById("wins").innerHTML(0);
+	var initialLoss = document.getElementById("losses").innerHTML(0);
+	var initialAttempt = document.getElementById("attempts").innerHTML(0);
+	console.log("Reset Scores!");
+	};
 
 
 
